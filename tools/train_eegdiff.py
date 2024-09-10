8667c2c017e3aee2d9be93f1ecf5ee6fe3fd799b
Zekun Jiang <37135971+JZK00@users.noreply.github.com>
2024-09-10 19:54:24 +0800

    Update train_eegdiff.py

--- tools/train_eegdiff.py
+++ tools/train_eegdiff.py
@@ -8,7 +8,7 @@ config = Config.fromfile(config_file_path)
 
 trainner = EEGDiffMR.build(config.trainner)
 
-wandb.login(key=None) #key
+wandb.login(key=None) #key   using your wandb KEY!
 wandb.init(
     # set the wandb project where this run will be logged
     project=config.project_name,
